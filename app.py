b46d182ee2da3f03482fdedab577652a94035dee
juto-shogan <somtombonnu53@gmail.com>
2025-05-30 17:57:23 +0100

    stage1

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+st.title("Electric Vehicle Market Analysis")
